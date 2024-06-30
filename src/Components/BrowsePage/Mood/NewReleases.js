014af36e25cf0fdb44ab6a7cbbb20a8e3a318f83
Abhishek Chauhan <abhishekb.tech271199@gmail.com>
2024-06-30 11:30:09 +0530

    update NewReleases

--- src/Components/BrowsePage/Mood/NewReleases.js
+++ src/Components/BrowsePage/Mood/NewReleases.js
@@ -41,10 +41,6 @@ function NewReleases() {
         });
     };
 
-    if (error) {
-        return <div>Error: {error}</div>;
-    }
-
     return (
         <>
             <h2 className="text-xl font-semibold mb-2 ml-9">New Releases</h2>
