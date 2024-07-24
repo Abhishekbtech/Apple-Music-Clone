6772fdbebca9fd47ae6beccf4c4e521ff59433d7
Abhishek Chauhan <abhishekb.tech271199@gmail.com>
2024-07-24 22:52:41 +0530

    update

--- src/Components/BrowsePage/BrowsePage.js
+++ src/Components/BrowsePage/BrowsePage.js
@@ -1,4 +1,5 @@
 import React, { Suspense } from 'react';
+import Footer from './Footer/Footer';
 
 // Lazy load mood components and Artists component
 const Romantic = React.lazy(() => import('./Mood/Romantic'));
@@ -11,32 +12,35 @@ const Artists = React.lazy(() => import('./Mood/Artists'));
 
 function BrowsePage() {
     return (
-        <div className="container mx-auto p-4 bg-gradient-to-br from-indigo-900 via-purple-400 to-purple-600 text-white">
-            <h1 className="text-2xl font-bold mb-4 pt-3 mt-11">Featured Music</h1>
-            <Suspense fallback={<div>Loading...</div>}>
-                <section className="mt-8">
-                    <NewReleases />
-                </section>
-                <section className="mt-8">
-                    <TopCharts />
-                </section>
-                <section className="mt-8">
-                    <Romantic />
-                </section>
-                <section className="mt-8">
-                    <Excited />
-                </section>
-                <section className="mt-8">
-                    <Happy />
-                </section>
-                <section className="mt-8">
-                    <Sad />
-                </section>
-                <section className="mt-8">
-                    <Artists />
-                </section>
-            </Suspense>
-        </div>
+        <>
+            <div className="container mx-auto p-4 bg-gradient-to-br from-indigo-900 via-purple-400 to-purple-600 text-white">
+                <h1 className="text-2xl font-bold mb-4 pt-3 mt-11">Featured Music</h1>
+                <Suspense fallback={<div>Loading...</div>}>
+                    <section className="mt-8">
+                        <NewReleases />
+                    </section>
+                    <section className="mt-8">
+                        <TopCharts />
+                    </section>
+                    <section className="mt-8">
+                        <Romantic />
+                    </section>
+                    <section className="mt-8">
+                        <Excited />
+                    </section>
+                    <section className="mt-8">
+                        <Happy />
+                    </section>
+                    <section className="mt-8">
+                        <Sad />
+                    </section>
+                    <section className="mt-8">
+                        <Artists />
+                    </section>
+                </Suspense>
+            </div>
+            <Footer />
+        </>
     );
 }
 
