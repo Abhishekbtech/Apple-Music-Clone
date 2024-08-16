4bc97dfd65cc98992174bbe54ce74e4dfd263328
Abhishek Chauhan <abhishekb.tech271199@gmail.com>
2024-08-16 11:14:11 +0530

    add toasts for all link

--- src/Components/BrowsePage/Footer/Footer.js
+++ src/Components/BrowsePage/Footer/Footer.js
@@ -1,33 +1,39 @@
 import React from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Footer = () => {
-    
+    const handleLinkClick = () => {
+        toast.info("Under Construction");
+    };
+
     return (
         <footer className="text-white mb-11">
             <div className="max-w-7xl mx-auto py-6 px-4">
                 <div className="flex flex-wrap justify-center space-x-4 space-y-2 sm:space-y-0 sm:space-x-4">
-                    <a href="#" className="hover:underline">United States</a>
-                    <a href="#" className="hover:underline">Español (México)</a>
-                    <a href="#" className="hover:underline">العربية</a>
-                    <a href="#" className="hover:underline">Русский</a>
-                    <a href="#" className="hover:underline">简体中文</a>
-                    <a href="#" className="hover:underline">Français (France)</a>
-                    <a href="#" className="hover:underline">한국어</a>
-                    <a href="#" className="hover:underline">Português (Brazil)</a>
-                    <a href="#" className="hover:underline">Tiếng Việt</a>
-                    <a href="#" className="hover:underline">繁體中文 (台灣)</a>
+                    <span onClick={handleLinkClick} className="hover:underline cursor-pointer">United States</span>
+                    <span onClick={handleLinkClick} className="hover:underline cursor-pointer">Español (México)</span>
+                    <span onClick={handleLinkClick} className="hover:underline cursor-pointer">العربية</span>
+                    <span onClick={handleLinkClick} className="hover:underline cursor-pointer">Русский</span>
+                    <span onClick={handleLinkClick} className="hover:underline cursor-pointer">简体中文</span>
+                    <span onClick={handleLinkClick} className="hover:underline cursor-pointer">Français (France)</span>
+                    <span onClick={handleLinkClick} className="hover:underline cursor-pointer">한국어</span>
+                    <span onClick={handleLinkClick} className="hover:underline cursor-pointer">Português (Brazil)</span>
+                    <span onClick={handleLinkClick} className="hover:underline cursor-pointer">Tiếng Việt</span>
+                    <span onClick={handleLinkClick} className="hover:underline cursor-pointer">繁體中文 (台灣)</span>
                 </div>
                 <div className="mt-6 text-center text-xs">
                     <p>Copyright © 2024 Apple Inc. All rights reserved.</p>
                     <div className="flex flex-wrap justify-center space-x-4 space-y-2 sm:space-y-0 sm:space-x-4 mt-2">
-                        <a href="#" className="hover:underline">Internet Service Terms</a>
-                        <a href="#" className="hover:underline">Apple Music & Privacy</a>
-                        <a href="#" className="hover:underline">Cookie Warning</a>
-                        <a href="#" className="hover:underline">Support</a>
-                        <a href="#" className="hover:underline">Feedback</a>
+                        <span onClick={handleLinkClick} className="hover:underline cursor-pointer">Internet Service Terms</span>
+                        <span onClick={handleLinkClick} className="hover:underline cursor-pointer">Apple Music & Privacy</span>
+                        <span onClick={handleLinkClick} className="hover:underline cursor-pointer">Cookie Warning</span>
+                        <span onClick={handleLinkClick} className="hover:underline cursor-pointer">Support</span>
+                        <span onClick={handleLinkClick} className="hover:underline cursor-pointer">Feedback</span>
                     </div>
                 </div>
             </div>
+            <ToastContainer />
         </footer>
     );
 };
